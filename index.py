08b6ab58bf8c591888dca264cd37f85f3dbe0a67
HaiAlison <maithanhhai9x@gmail.com>
2021-06-11 14:39:28 +0700

    add try catch

--- index.py
+++ index.py
@@ -24,10 +24,10 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 time.sleep(2)
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("user-agent=" + USER_AGENT)
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('disable-gpu')
-chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--window-size=1599,796")
 chrome_options.add_argument('ignore-certificate-errors')
 
@@ -67,94 +67,98 @@ for lat in lats:
     x = range(10, 1590, 30)
     for height in y:
         for width in x:
-            print(width, height)
-            action.move_to_element_with_offset(el, width, height)
-            action.click()
-            action.perform()
-            print(driver.requests[-2].url)
-            time.sleep(3)
-            if "/api/search/" in driver.requests[-2].url:
-                time.sleep(5)
-                if hasattr(driver.requests[-2].response,'body'):
-                    my_json = driver.requests[-2].response.body.decode('utf8','ignore').replace("'", '"')
-                    if "polygons" in my_json:
-                        print('ready to create data')
-                        area = json.loads(my_json)[0]['area']
-                        address = json.loads(my_json)[0]['address'].split(', ')  # số tờ, số thửa, phường, quận, tp
-                        print('length address', len(address))
-                        if len(address) >= 3:
-                            polygons = json.loads(my_json)[0]['polygons']
-                            addressIds = {}
-                            newMap = {}
-                            province = None
-                            district = None
-                            ward = None
-                            if "Hồ Chí Minh" in address[3]:
-                                query = """
-                                {
-                                    checkAddressByName(province:"TP.Hồ Chí Minh"
-                                    district:\"""" + address[2] + """\"
-                                    ward:\"""" + address[1] + """\"
-                                    street:"Đường") {
-                                        id
+            try:
+                print(width, height)
+                action.move_to_element_with_offset(el, width, height)
+                action.click()
+                action.perform()
+                print(driver.requests[-2].url)
+                time.sleep(3)
+                if "/api/search/" in driver.requests[-2].url:
+                    time.sleep(5)
+                    if hasattr(driver.requests[-2].response,'body'):
+                        my_json = driver.requests[-2].response.body.decode('utf8','ignore').replace("'", '"')
+                        if "polygons" in my_json:
+                            print('ready to create data')
+                            area = json.loads(my_json)[0]['area']
+                            address = json.loads(my_json)[0]['address'].split(', ')  # số tờ, số thửa, phường, quận, tp
+                            print('length address', len(address))
+                            if len(address) >= 3:
+                                polygons = json.loads(my_json)[0]['polygons']
+                                addressIds = {}
+                                newMap = {}
+                                province = None
+                                district = None
+                                ward = None
+                                if "Hồ Chí Minh" in address[3]:
+                                    query = """
+                                    {
+                                        checkAddressByName(province:"TP.Hồ Chí Minh"
+                                        district:\"""" + address[2] + """\"
+                                        ward:\"""" + address[1] + """\"
+                                        street:"Đường") {
+                                            id
+                                        }
                                     }
-                                }
-                                """
-                                addressIds = run_query(query)
-                                province = addressIds['data']['checkAddressByName'][0]['id']
-                                district = addressIds['data']['checkAddressByName'][1]['id']
-                                ward = addressIds['data']['checkAddressByName'][2]['id']
-                            else:
-                                mapMutation = gql(
                                     """
-                                    mutation createMap($province: String!, $district: String!, $ward: String!, $street: String){
-                                        createMap(
-                                            province: $province
-                                            district: $district
-                                            ward: $ward
-                                            street: $street
+                                    addressIds = run_query(query)
+                                    province = addressIds['data']['checkAddressByName'][0]['id']
+                                    district = addressIds['data']['checkAddressByName'][1]['id']
+                                    ward = addressIds['data']['checkAddressByName'][2]['id']
+                                else:
+                                    mapMutation = gql(
+                                        """
+                                        mutation createMap($province: String!, $district: String!, $ward: String!, $street: String){
+                                            createMap(
+                                                province: $province
+                                                district: $district
+                                                ward: $ward
+                                                street: $street
+                                                ) {
+                                                province
+                                                district
+                                                ward
+                                                street
+                                                }
+                                            }
+                                        """
+                                    )
+                                    mapData = {"province": address[3], "district": address[2], "ward": address[1]}
+                                    newMap = client.execute(mapMutation, variable_values=mapData)
+                                    province = newMap['createMap']['province']
+                                    district = newMap['createMap']['district']
+                                    ward = newMap['createMap']['ward']
+                                time.sleep(1)
+                                pages = address[0].split(' ')[1]
+                                lot = address[0].split(' ')[3]
+                                mutation = gql(
+                                    """
+                                    mutation createLand($polygons: [[[Float]]] ,$provinceId: ObjectID! $area: Float $districtId: ObjectID!, $wardId: ObjectID!, $streetId: ObjectID, $landNumber: Int!, $mapNumber: Int!){
+                                        createLand(
+                                            polygon: {
+                                                type:Polygon,
+                                                coordinates: $polygons
+                                            }
+                                            provinceId: $provinceId
+                                            districtId: $districtId
+                                            wardId: $wardId
+                                            area: $area
+                                            streetId: $streetId
+                                            landNumber: $landNumber
+                                            mapNumber: $mapNumber
                                             ) {
-                                            province
-                                            district
-                                            ward
-                                            street
+                                            id
                                             }
                                         }
                                     """
                                 )
-                                mapData = {"province": address[3], "district": address[2], "ward": address[1]}
-                                newMap = client.execute(mapMutation, variable_values=mapData)
-                                province = newMap['createMap']['province']
-                                district = newMap['createMap']['district']
-                                ward = newMap['createMap']['ward']
-                            time.sleep(1)
-                            pages = address[0].split(' ')[1]
-                            lot = address[0].split(' ')[3]
-                            mutation = gql(
-                                """
-                                mutation createLand($polygons: [[[Float]]] ,$provinceId: ObjectID! $area: Float $districtId: ObjectID!, $wardId: ObjectID!, $streetId: ObjectID, $landNumber: Int!, $mapNumber: Int!){
-                                    createLand(
-                                        polygon: {
-                                            type:Polygon,
-                                            coordinates: $polygons
-                                        }
-                                        provinceId: $provinceId
-                                        districtId: $districtId
-                                        wardId: $wardId
-                                        area: $area
-                                        streetId: $streetId
-                                        landNumber: $landNumber
-                                        mapNumber: $mapNumber
-                                        ) {
-                                        id
-                                        }
-                                    }
-                                """
-                            )
-                            variables = {"polygons": polygons[0], "area": area, "provinceId": province, "districtId": district,
-                                         "wardId": ward, "landNumber": int(lot), "mapNumber": int(pages)}
-                            result = client.execute(mutation, variable_values=variables)
-                            print(result)
+                                variables = {"polygons": polygons[0], "area": area, "provinceId": province, "districtId": district,
+                                             "wardId": ward, "landNumber": int(lot), "mapNumber": int(pages)}
+                                result = client.execute(mutation, variable_values=variables)
+                                print(result)
+            except Exception as e:
+                print(e)
+                continue
     driver.quit()
 
 
